8a4ce00b6dc59d8b73a9f9b221789b2ef21ff02a
Jared Hanson <jaredhanson@gmail.com>
2018-07-16 17:30:06 -0700

    Add tests.

--- test/connection.test.js
+++ test/connection.test.js
@@ -37,7 +37,7 @@ describe('Connection', function() {
       var connection = new Connection({ projectId: 'example' });
   
       before(function() {
-        sinon.stub(publisherObj, 'publish').yields(null);
+        sinon.stub(publisherObj, 'publish').yields(null, 1234);
         sinon.stub(topicObj, 'publisher').returns(publisherObj);
       });
     
@@ -58,11 +58,67 @@ describe('Connection', function() {
         expect(connection._client.options).to.deep.equal({ projectId: 'example' });
       });
   
-      it('should attempt to publish message', function() {
+      it('should publish message', function() {
         expect(publisherObj.publish.callCount).to.equal(1);
         expect(publisherObj.publish.args[0][0]).to.deep.equal(Buffer.from('Hello, world!'));
       });
     }); // successfully publishing message
+    
+    describe('encountering an error publishing message', function() {
+      var topicObj = {
+        publisher: function() {}
+      };
+      var publisherObj = {
+        publish: function() {}
+      };
+    
+      function MockPubSub(options) {
+        this.options = options;
+      }
+      MockPubSub.prototype.topic = function(topic) {
+        if (topic !== 'my-topic') { throw new Error('Invalid topic: ' + topic) };
+        return topicObj;
+      }
+    
+    
+      var Connection = $require('../lib/connection',
+        { '@google-cloud/pubsub': MockPubSub });
+      var connection = new Connection({ projectId: 'example' });
+  
+      before(function() {
+        sinon.stub(publisherObj, 'publish').yields(new Error('something went wrong'));
+        sinon.stub(topicObj, 'publisher').returns(publisherObj);
+      });
+    
+      after(function() {
+        topicObj.publisher.restore();
+        publisherObj.publish.restore();
+      });
+  
+      var error;
+      before(function(done) {
+        connection.connect(function() {
+          connection.publish('my-topic', { body: 'Hello, world!' }, function(err) {
+            error = err;
+            done();
+          })
+        });
+      })
+      
+      it('should construct client with correct options', function() {
+        expect(connection._client.options).to.deep.equal({ projectId: 'example' });
+      });
+  
+      it('should attempt to publish message', function() {
+        expect(publisherObj.publish.callCount).to.equal(1);
+        expect(publisherObj.publish.args[0][0]).to.deep.equal(Buffer.from('Hello, world!'));
+      });
+      
+      it('should yield error', function() {
+        expect(error).to.be.an.instanceof(Error);
+        expect(error.message).to.equal('something went wrong');
+      });
+    }); // encountering an error publishing message
   
   }); // #publish
   
